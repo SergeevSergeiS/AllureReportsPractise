90410e48a096cc2bbc3bc31ec64770c086a995d8
SergeevSS90 <sergeevss90@internet.ru>
2022-04-17 00:15:25 +0400

    Work on Lambda

--- src/test/java/ru/internet/sergeevss90/tests/SelenideTestWithLambda.java
+++ src/test/java/ru/internet/sergeevss90/tests/SelenideTestWithLambda.java
@@ -1,29 +1,38 @@
 package ru.internet.sergeevss90.tests;
 
-import com.codeborne.selenide.*;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 import static org.openqa.selenium.By.partialLinkText;
 
-public class SelenideTest {
+public class SelenideTestWithLambda {
 
     SelenideElement search = $(".header-search-input");
-    String searchTask = "SergeevSergeiS/qa_guru_12_1_git";
+    private static final String searchTask = "SergeevSergeiS/qa_guru_12_1_git";
 
     @BeforeAll
     static void prepare() {
-        Configuration.holdBrowserOpen = true;
-        Configuration.baseUrl = "https://github.com";
-        Configuration.browserSize = "1920x1080";
+        step("Настраиваем конфигурацию браузера", () -> {
+            Configuration.baseUrl = "https://github.com";
+            Configuration.browserSize = "1920x1080";
+        });
     }
 
     @Test
     public void testGithubIssue() {
-        open("");
+        step("Открываем главную страницу:" + Configuration.baseUrl, () -> {
+            open("");
+        });
+
 
         search.click();
         search.sendKeys(searchTask);
@@ -32,7 +41,5 @@ public class SelenideTest {
         $(linkText(searchTask)).click();
         $(partialLinkText("Issues")).click();
         $(withText("#2")).click();
-
-
     }
 }
---
+++ src/test/java/ru/internet/sergeevss90/tests/SelenideTestWithListener.java
@@ -0,0 +1,38 @@
+package ru.internet.sergeevss90.tests;
+
+import com.codeborne.selenide.*;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class SelenideTestWithListener {
+
+    SelenideElement search = $(".header-search-input");
+    String searchTask = "SergeevSergeiS/qa_guru_12_1_git";
+
+    @BeforeAll
+    static void prepare() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    public void testGithubIssue() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open("");
+
+        search.click();
+        search.sendKeys(searchTask);
+        search.submit();
+
+        $(linkText(searchTask)).click();
+        $(partialLinkText("Issues")).click();
+        $(withText("#2")).click();
+    }
+}
